d147f50238961d16f6f94cdd8e7c38fbc3a45cd1
Andre coelho <andre.coelho.santos@hotmail.com>
2017-01-28 12:57:20 -0200

    inserido o conteudo da index

--- site/index.php
+++ site/index.php
@@ -5,6 +5,9 @@
 	<title>Curso de git</title>
 </head>
 <body>
-	
+	<h2>Lorem Ipsum</h2>
+
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at bibendum ante. Quisque at consequat purus. Morbi vitae ultrices sapien, quis tincidunt felis. Sed vestibulum nec nisi vitae congue. Nunc eu ultricies leo, non malesuada mi. Proin et fringilla ligula, sit amet tincidunt turpis. Sed ac vestibulum ante. Quisque rutrum turpis eget leo mattis aliquam condimentum nec nisl. Nam viverra tellus libero, vel aliquam neque rhoncus ac. Aliquam efficitur lacus vitae ante molestie, sed commodo nulla ornare. Duis eget arcu id dolor ornare sollicitudin. Nullam convallis mi nec felis blandit vestibulum.</p>
+	<p>Mauris libero nisi, molestie sit amet dolor sit amet, varius feugiat arcu. Etiam tristique, elit vel lobortis varius, lacus neque feugiat lectus, eget dictum nibh orci ac justo. Praesent ipsum massa, pharetra ut elit eu, elementum tempor quam. Praesent at lacus vel ex mattis vehicula sit amet id dui. Maecenas mattis convallis mauris eget porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut tincidunt neque eu nulla mattis, at semper nisl elementum. Vivamus tellus arcu, blandit imperdiet blandit a, tristique sit amet metus. Nulla hendrerit sed nisi id ultrices.</p>
 </body>
 </html>
